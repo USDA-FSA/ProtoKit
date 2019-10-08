7fb49f970f3f09da80aedb651e0b857fc32abbb4
Casey Hill <bignamehere@gmail.com>
2019-10-08 17:28:13 -0500

    removed search from router

--- src/_router/routes.js
+++ src/_router/routes.js
@@ -7,7 +7,6 @@ const Home = () => import('../views/Home');
 const Boilerplate = () => import('../views/Boilerplate');
 const ModalGrowl = () => import('../views/demos/Modal-Growl');
 const Table = () => import('../views/demos/Table');
-const Search = () => import('../views/demos/Search');
 const ReportsOverview = () => import('../views/reports/Overview');
 const InvestmentPlacement = () => import('../views/reports/Investment-Placement');
 
